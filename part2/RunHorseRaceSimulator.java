d0978c7fe9dcd3a8065d262e66db2441ccb67301
Jadid Alam <ec23119@qmul.ac.uk>
2024-04-23 13:18:07 +0100

    Initial commit

---
+++ part2/RunHorseRaceSimulator.java
@@ -0,0 +1,48 @@
+/**
+ * Java Porgram to set up and simulate the horse race
+ * 
+ * @author Jadid Alam
+ * @version 1.1
+ */
+
+ import java.util.Random;
+ import java.util.Scanner;
+ 
+ class RunHorseRaceSimulator
+ {
+     public static void main(String[] args)
+     {
+         // initialised horses and sets the distance
+         char symbol1 = 'B';
+         char symbol2 = 'G';
+         char symbol3 = 'H';
+         int distance = 50;
+ 
+         Random random = new Random();
+ 
+         // initialised the horses
+         Horse horse1 = new Horse(symbol1,"barry",(random.nextInt(8)+1)/10.0);
+         Horse horse2 = new Horse(symbol2,"garry",(random.nextInt(8)+1)/10.0);
+         Horse horse3 = new Horse(symbol3,"harry", (random.nextInt(8)+1)/10.0);
+ 
+         // initialised the race
+         Race race = new Race(distance);
+ 
+         // adding horse to the race
+         race.addHorse(horse1, 1);
+         race.addHorse(horse2, 2);
+         race.addHorse(horse3, 3);
+ 
+         Scanner s = new Scanner(System.in);
+ 
+         // displaying the race on the terminal and giving an option to start another race
+         boolean reapetRace = true;
+         while (reapetRace)
+         {
+             race.startRace();
+ 
+             System.out.println("Start another race? (y/n)");
+             reapetRace = s.nextLine().toLowerCase().charAt(0) == 'y';
+         }
+     }
+ }
