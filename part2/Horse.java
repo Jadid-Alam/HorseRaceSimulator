3da2d8d04514d61ebb59568a5dc8632182d9d880
Jadid Alam <ec23119@qmul.ac.uk>
2024-04-23 12:32:29 +0100

    Initial commit

---
+++ part2/Horse.java
@@ -0,0 +1,82 @@
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author Jadid
+ * @version 1.0
+ */
+public class Horse
+{
+    //Fields of class Horse
+    private char symbol;
+    private String name;
+    private double confidence;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        this.hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return this.confidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return this.distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public char getSymbol()
+    {
+        return this.symbol;
+    }
+    
+    public void goBackToStart()
+    {
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+    }
+    
+    public boolean hasFallen()
+    {
+        return this.hasFallen;
+    }
+
+    public void moveForward()
+    {
+        this.distanceTravelled++;
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        this.confidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        this.symbol = newSymbol;
+    }
+    
+}
