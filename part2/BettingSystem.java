fff7ce9dd09c1597f0abedd41fa392eca8a87f3b
Jadid Alam <ec23119@qmul.ac.uk>
2024-04-23 16:11:17 +0100

    @author: Jadid Alam @version: 1.2 @date: 23/01/2024
    - added comments
    - (older version) added a new method called 'moveBackward' to move the horse backward

--- part2/BettingSystem.java
+++ part2/BettingSystem.java
@@ -1,3 +1,12 @@
+/**
+ * A class that programms a betting system for the
+ * game to use. It allows the user to place a bet
+ * and win or lose based on the outcome.
+ * 
+ * @author Jadid Alam
+ * @version 1.1
+ */
+
 import java.util.Random;
 
 public class BettingSystem {
@@ -8,13 +17,15 @@ public class BettingSystem {
         this.storage = storage;
     }
 
+    // place a bet on a horse with a certain amount and number of the horse
     public void bet(int horseNumber, int betAmount) {
         storage.setBetAmount(betAmount);
         storage.setBetOnHorseNumber(horseNumber);
     }
 
+    // calculate the odds of winning for each horse
     public void calculateOdds() {
-        // calculate odds
+        // getting the highest confidence and multiplying it by 10 to get a whole number
         int highestNumber = 0;
         for (int i = 0; i < storage.getNoOfHorses(); i++) {
             if (storage.getHorses(i).getConfidence()*10 > highestNumber)
@@ -23,6 +34,11 @@ public class BettingSystem {
             }
         }
 
+        /*
+         * setting the odds of winning for each horse by finding the difference 
+         * between the highest confidence and the horse's confidence and adding
+         * a minimum number based on the track conditions
+         */
         for (int i = 0; i < storage.getNoOfHorses(); i++) {
             int confidence = (int) (storage.getHorses(i).getConfidence()*10);
             int minOdds = 0;
@@ -44,25 +60,31 @@ public class BettingSystem {
         }
     }
 
+    // determine if the bet was won or lost
     public void betWonOrLost() {
 
-        // generate random number
+        // generate random number to give random feedback
         Random random = new Random();
         counter = random.nextInt(5);
 
+        // if the horse is the winner and the user has placed a bet
         if (storage.getWinnerIndex() == storage.getBetOnHorseNumber() && storage.getBetAmount() != 0)
         {
+            // set the wins and current balance
             storage.setWins(storage.getWins() + 1);
             storage.setCurrentBalance(storage.getCurrentBalance() + storage.getBetAmount() * (storage.getHorses(storage.getBetOnHorseNumber()).getOddsOfWinning()+1));
             storage.getBalanceLabel().setText(" Balance: " + storage.getCurrentBalance());
         }
         else if (storage.getBetAmount() != 0)
         {
+            // set the losses
             storage.setLosses(storage.getLosses() + 1);
         }
 
+        // if the horse is the winner and the user has placed a bet
         if (storage.getWinnerIndex() == storage.getBetOnHorseNumber() && storage.getBetAmount() != 0)
         {
+            // give feedback based on the counter
             switch (counter) {
                 case 0:
                     storage.setFeedback("Well done! Stick to your strategy and don't get greedy.");
@@ -84,10 +106,12 @@ public class BettingSystem {
         }
         else if (storage.getBetAmount() == 0)
         {
+            // if the user has not placed a bet
             storage.setFeedback("You have not placed a bet. So no feedback is available.");
         }
         else
         {
+            // if the horse is not the winner and the user has placed a bet give feedback based on the counter
             switch (counter) {
                 case 0:
                     storage.setFeedback("Unlucky! Remember that the more confident the horse, the lower the odds.");
--- part2/Horse.java
+++ part2/Horse.java
@@ -1,9 +1,8 @@
-
 /**
  * Write a description of class Horse here.
  * 
  * @author Jadid
- * @version 1.1
+ * @version 1.2
  */
 public class Horse
 {
@@ -26,68 +25,80 @@ public class Horse
         this.confidence = horseConfidence;
     }
     
-    //Other methods of class Horse
+    // return the odds of winning for the betting system
     public int getOddsOfWinning()
     {
         return this.oddsOfWinning;
     }
 
+    // set the odds of winning for the betting system
     public void setOddsOfWinning(int newOdds)
     {
         this.oddsOfWinning = newOdds;
     }
 
+    // return whether the horse is the winner
     public boolean isWinner()
     {
         return this.isWinner;
     }
 
+    // set whether the horse is the winner
     public void setWinner(boolean winner)
     {
         this.isWinner = winner;
     }
 
+    // sets the hasFallen field to true
     public void fall()
     {
         this.hasFallen = true;
     }
     
+    // returns the confidence of the horse
     public double getConfidence()
     {
         return this.confidence;
     }
     
+    // returns the distance travelled by the horse
     public int getDistanceTravelled()
     {
         return this.distanceTravelled;
     }
     
+    // returns the name of the horse
     public String getName()
     {
         return this.name;
     }
     
+    // sets the distance travelled by the horse to 0 and hasFallen to false
     public void goBackToStart()
     {
         this.distanceTravelled = 0;
         this.hasFallen = false;
     }
     
+    // returns whether the horse has fallen
     public boolean hasFallen()
     {
         return this.hasFallen;
     }
 
+    // moves the horse forward by 1
     public void moveForward()
     {
         this.distanceTravelled++;
     }
 
+    // moves the horse backward by 1
     public void moveBackward()
     {
         this.distanceTravelled--;
     }
 
+    // sets the confidence of the horse
     public void setConfidence(double newConfidence)
     {
         this.confidence = newConfidence;
